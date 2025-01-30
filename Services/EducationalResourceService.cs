ed28a169a0a85cddbdd8057692f67d46279f773b
ETjoel <ejoeltesfaye@gmail.com>
2025-01-30 03:40:53 -0800

    updated services

--- Services/EducationalResourceService.cs
+++ Services/EducationalResourceService.cs
@@ -1,30 +1,34 @@
 using EducationalResourceAPI.Models;
-using EducationalResourceAPI.Repositories;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 
 namespace EducationalResourceAPI.Services
 {
-    public class EducationalResourceService
+    public class EducationalResourceService : IEducationalResourceService
     {
-        private readonly EducationalResourceRepository _repository;
+        private readonly IMongoCollection<EducationalResource> _resources;
 
-        public EducationalResourceService(EducationalResourceRepository repository)
+        public EducationalResourceService(IOptions<DatabaseSettings> mongoSettings)
         {
-            _repository = repository;
+            var client = new MongoClient(mongoSettings.Value.ConnectionString);
+            var database = client.GetDatabase(mongoSettings.Value.DatabaseName);
+            _resources = database.GetCollection<EducationalResource>("Resources");
         }
 
         public async Task<List<EducationalResource>> GetAllResourcesAsync() =>
-            await _repository.GetAllResourcesAsync();
+            await _resources.Find(resource => true).ToListAsync();
 
         public async Task<EducationalResource?> GetResourceByIdAsync(string id) =>
-            await _repository.GetResourceByIdAsync(id);
+            await _resources.Find(resource => resource.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateResourceAsync(EducationalResource resource) =>
-            await _repository.CreateResourceAsync(resource);
+            await _resources.InsertOneAsync(resource);
 
         public async Task UpdateResourceAsync(string id, EducationalResource resource) =>
-            await _repository.UpdateResourceAsync(id, resource);
+            await _resources.ReplaceOneAsync(r => r.Id == id, resource);
 
         public async Task DeleteResourceAsync(string id) =>
-            await _repository.DeleteResourceAsync(id);
+            await _resources.DeleteOneAsync(resource => resource.Id == id);
     }
 }
